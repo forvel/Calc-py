3a114c0ef2eb975069caaf9bc95baa5e75e1f178
Pavel Nikiforov <velfor@tut.by>
2020-01-07 11:51:45 +0300

    Some fixes in interface

--- calc.py
+++ calc.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from tkinter import *
 
 root = Tk()
@@ -8,18 +7,18 @@ root.grid_columnconfigure(4, weight=1)
 
 lblNumber1 = Label(root, text="Число 1", width = 10, anchor = W)
 lblNumber1.grid(row = 0, column = 0, columnspan = 2, sticky = W)
-entNumber1 = Entry(root, width = 12, justify = RIGHT)
-entNumber1.grid(row = 0, column = 2, columnspan = 2, sticky = W)
+entNumber1 = Entry(root, width = 13, justify = RIGHT)
+entNumber1.grid(row = 0, column = 2, columnspan = 2, sticky = E, padx=2)
 
 lblNumber2 = Label(root, text="Число 2", width = 10, anchor = W)
 lblNumber2.grid(row = 1, column = 0, columnspan = 2, sticky = W)
-entNumber2 = Entry(root, width = 12, justify = RIGHT)
-entNumber2.grid(row = 1, column = 2, columnspan = 2, sticky = W)
+entNumber2 = Entry(root, width = 13, justify = RIGHT)
+entNumber2.grid(row = 1, column = 2, columnspan = 2, sticky = E, padx=2)
 
 lblResult = Label(root, text="Результат", width = 10, anchor = W)
 lblResult.grid(row = 2, column = 0, columnspan = 2, sticky = W)
-entResult = Entry(root, width = 12, justify = RIGHT)
-entResult.grid(row = 2, column = 2, columnspan = 2, sticky = W)
+entResult = Entry(root, width = 13, justify = RIGHT)
+entResult.grid(row = 2, column = 2, columnspan = 2, sticky = E, padx=2)
 
 btnList =   (
             ('7','8','9','/'),
@@ -32,38 +31,4 @@ for i in range(4):
         btn = Button (root,text = btnList[i][j],width = 5)
         btn.grid(row = i+3, column = j,padx=2,pady=2)
 root.mainloop()
-=======
-from tkinter import *
 
-root = Tk()
-root.title("Калькулятор")
-root.grid_rowconfigure(7, weight=1)
-root.grid_columnconfigure(4, weight=1)
-
-lblNumber1 = Label(root, text="Число 1", width = 10, anchor = W)
-lblNumber1.grid(row = 0, column = 0, columnspan = 2, sticky = W)
-entNumber1 = Entry(root, width = 12, justify = RIGHT)
-entNumber1.grid(row = 0, column = 2, columnspan = 2, sticky = W)
-
-lblNumber2 = Label(root, text="Число 2", width = 10, anchor = W)
-lblNumber2.grid(row = 1, column = 0, columnspan = 2, sticky = W)
-entNumber2 = Entry(root, width = 12, justify = RIGHT)
-entNumber2.grid(row = 1, column = 2, columnspan = 2, sticky = W)
-
-lblResult = Label(root, text="Результат", width = 10, anchor = W)
-lblResult.grid(row = 2, column = 0, columnspan = 2, sticky = W)
-entResult = Entry(root, width = 12, justify = RIGHT)
-entResult.grid(row = 2, column = 2, columnspan = 2, sticky = W)
-
-btnList =   (
-            ('7','8','9','/'),
-            ('4','5','6','*'),
-            ('1','2','3','-'),
-            ('0',',','+','=')
-            )
-for i in range(4):
-    for j in range(4):
-        btn = Button (root,text = btnList[i][j],width = 5)
-        btn.grid(row = i+3, column = j,padx=2,pady=2)
-root.mainloop()
->>>>>>> a7b7cafc19a3b54d8efa763a8d27c262c00b7f24
